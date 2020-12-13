5c91f9675ee83f349562e9f7962793cf5796a991
Hwannam <66862804+Hwannam@users.noreply.github.com>
2020-12-14 07:55:33 +0900

    Add files via upload

---
+++ lab3/q3_a/ItemType.h
@@ -0,0 +1,31 @@
+// ItemType.h  contains the specifications for the data for the real 
+//  estate manipulation program.
+
+
+const int MAX_ITEMS = 25;
+#include <fstream> 
+#include "StrTypeAugmented.h"   // Gain access to String ADT.
+enum RelationType { LESS, EQUAL, GREATER };
+class HouseType
+{
+public:
+	void GetFromFile(std::ifstream&);
+	void WriteToFile(std::ofstream&);
+	void GetFromUser();
+	void PrintHouseToScreen();
+	void GetNameFromUser();
+	void PrintNameToScreen();
+	RelationType ComparedTo(HouseType);
+
+private:
+	StrType lastName;
+	StrType firstName;
+	StrType address;
+	float price;
+	int squareFeet;
+	int bedRooms;
+	int bathrooms;
+};
+
+// Make ItemType an alias for HouseType. 
+typedef HouseType ItemType;
